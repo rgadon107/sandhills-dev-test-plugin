7990c7c92922fc848a26dfc3918c184f0731c631
Robert Gadon <rgadon107@gmail.com>
2020-02-11 00:15:32 -0500

    Add shortcode processing and http response request functions.
    - The processing function loads the  config, merges the shortcode attributes, loads the http request function, starts the output buffer, calls the shortcode view file, and returns the buffer output to WP. 
    - The helper function makes an HTTP request to a remote ip address and returns the ip address of the caller.

--- src/shortcode/shortcode.php
+++ src/shortcode/shortcode.php
@@ -12,6 +12,7 @@
 namespace spiralWebDB\Sandhills;
 
 function register_the_shortcode( $config = [] ) {
+
 	$config = require_once _get_plugin_directory() . '/config/shortcode.php';
 
 	if ( ! is_array( $config ) || empty( $config ) ) {
@@ -25,8 +26,52 @@ function register_the_shortcode( $config = [] ) {
 	add_shortcode( $config['shortcode_name'], __NAMESPACE__ . '\process_the_shortcode' );
 }
 
-function process_the_shortcode() {
+/**
+ *  Process and render the HTML for the shortcode.
+ *
+ * @since 1.0.0
+ *
+ * @param array|string $user_defined_attributes User defined attributes for this shortcode instance.
+ * @param string|null  $content                 Content between the opening and closing shortcode elements.
+ * @param string       $shortcode_name          Name of the shortcode.
+ *
+ * @return string
+ *
+ */
+function process_the_shortcode( $user_defined_attributes, $content, $shortcode_name ) {
 
+	$config = require_once _get_plugin_directory() . '/config/shortcode.php';
+
+	$attributes = shortcode_atts(
+		$config['defaults'],
+		$user_defined_attributes,
+		$shortcode_name
+	);
+
+
+	if ( $content && $config['do_shortcode_within_content'] ) {
+		$content = do_shortcode( $content );
+	}
+
+	get_remote_ip_address( $attributes );
+
+	// Call the view file, capture it into the output buffer, and then return it.
+	ob_start();
+	include $config['view'];
+
+	return ob_get_clean();
 }
 
+/*
+ * Get the body (ip address) of an http response request.
+ *
+ * @since 1.0.0
+ * @param array $attributes Combined and filtered attribute list.
+ *
+ * @return array $request   The http request response.
+ */
+function get_remote_ip_address( $attributes ) {
+	$request = wp_remote_get( $attributes['url'] );
 
+	return $request['body'];
+}

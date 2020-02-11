d58a534c199bea2e21fae578d5bd4dbbde7f5b46
Robert Gadon <rgadon107@gmail.com>
2020-02-10 23:15:56 -0500

    Update HTML in the plugin single-product template file.

--- src/template/single-product.php
+++ src/template/single-product.php
@@ -24,9 +24,13 @@ get_header();
 			while ( have_posts() ) { ?>
 				<article class="entry single-product-<?php the_ID(); ?>">
 				<?php the_post(); ?>
-				<h2 class="entry-title"><?php the_title(); ?></h2>
-				<div class="entry-content"><?php the_content();
-			} ?></div>
+					<header class="entry-header has-text-align-center header-footer-group">
+						<div class="entry-header-inner section-inner medium">
+							<h2 class="entry-title"><?php the_title(); ?></h2><!-- .entry-header-inner -->
+								<div class="entry-content"><?php the_content();
+							} ?></div>
+						</div>
+					</header>
 				</article>
 		<?php } ?>
 	</main><!-- #site-content -->

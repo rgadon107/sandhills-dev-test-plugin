a2bf2d1faee51a7d8b3aae89e892ea6289f06cf5
Robert Gadon <rgadon107@gmail.com>
2020-02-11 21:13:01 -0600

    Runtime configuration for all the custom post types to be registered.
    - This file loads 1 or more runtime custom post type configurations. 
    - The configuration array is called from the custom post type registration handler ( post-type-registrar.php ).

---
+++ src/config/custom-post-types.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Runtime configuration for all the custom post types.
+ *
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @package    spiralWebDB\Sandhills
+ * @link       http://spiralwebdb.com
+ * @license    GNU-2.0+
+ */
+
+namespace spiralWebDB\Sandhills;
+
+return [
+	require __DIR__ . '/product-post-type.php',
+];
+

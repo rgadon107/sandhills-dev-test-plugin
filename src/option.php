7b6a3838929632e905b7ad3b34f1697c19ac5eab
Robert Gadon <rgadon107@gmail.com>
2020-02-12 11:48:38 -0600

    Move 'get_remote_ip_address' to option handler and set transient.

--- src/option.php
+++ src/option.php
@@ -11,4 +11,21 @@
 
 namespace spiralWebDB\Sandhills;
 
+/*
+ * Call remote IP address.
+ *
+ * @since 1.0.0
+ * @return string $address  The IP address returned by the http GET request.
+ */
+function get_remote_ip_address() {
+	$url      = 'http://bot.whatismyipaddress.com/';
+	$response = wp_remote_get( $url );
+	$address  = $response['body'];
+
+	return $address;
+}
+
+$value = get_remote_ip_address();
+
+set_transient( 'sandhills_get_remote_ip_address', $value, 3600 );
 

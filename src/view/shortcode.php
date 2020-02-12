e0d1c39b6675fba8eecdea1b4b8369f114abfc05
Robert Gadon <rgadon107@gmail.com>
2020-02-12 15:30:52 -0600

    PHPCS fixes.
    - Add @package declaration to file header.
    - Add blank lines before and after NS declaration.
    - Escape output of 'get_remote_ip_address'.

--- src/view/shortcode.php
+++ src/view/shortcode.php
@@ -1,7 +1,11 @@
 <?php
 /**
  *  View file for the registered shortcode.
+ *
+ * @package spiralWebDB\Sandhills
  */
+
 namespace spiralWebDB\Sandhills;
+
 ?>
-<p>The caller's IP address is: <?php echo get_remote_ip_address(); ?></p>
+<p>The caller's IP address is: <?php echo esc_html( get_remote_ip_address() ); ?></p>

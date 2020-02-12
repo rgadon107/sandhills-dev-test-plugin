5bd8a37be9d13a2e21a961b401ac88cf060daddc
Robert Gadon <rgadon107@gmail.com>
2020-02-12 10:51:54 -0600

    Add 'get_remote_ip_address', and call the function.

--- src/shortcode.php
+++ src/shortcode.php
@@ -8,6 +8,7 @@
  * @link       http://spiralwebdb.com
  * @license    GNU General Public License 2.0+
  */
+
 namespace spiralWebDB\Sandhills;
 
 /*
@@ -15,7 +16,7 @@ namespace spiralWebDB\Sandhills;
  *
  * @since 1.0.0
  */
-add_shortcode( 'feature' , __NAMESPACE__ . '\process_product_feature_shortcode' );
+add_shortcode( 'feature', __NAMESPACE__ . '\process_product_feature_shortcode' );
 
 /*
  * Processing callback for the [feature] shortcode.
@@ -29,3 +30,18 @@ function process_product_feature_shortcode() {
 	ob_get_clean();
 }
 
+/*
+ * Call remote IP address.
+ *
+ * @since 1.0.0
+ * @return string $address  The IP address returned by the http GET request.
+ */
+function get_remote_ip_address() {
+	$url      = 'http://bot.whatismyipaddress.com/';
+	$response = wp_remote_get( $url );
+	$address  = $response['body'];
+
+	return $address;
+}
+
+get_remote_ip_address();

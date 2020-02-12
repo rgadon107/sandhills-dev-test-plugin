876260665b26112e380886991b2a1ea914ac6377
Robert Gadon <rgadon107@gmail.com>
2020-02-12 10:53:04 -0600

    Update view file to echo the return value of 'get_remote_ip_address'.

--- src/view/shortcode.php
+++ src/view/shortcode.php
@@ -1,13 +1,7 @@
 <?php
 /**
  *  View file for the registered shortcode.
- *
- * @package    spiralWebDB\Sandhills;
- * @since      1.0.0
- * @author     Robert A. Gadon
- * @link       http://spiralwebdb.com
- * @license    GNU General Public License 2.0+
  */
 namespace spiralWebDB\Sandhills;
 ?>
-<p>You called the shortcode view file.</p>
+<p>The caller's IP address is: <?php echo get_remote_ip_address(); ?></p>

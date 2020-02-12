c49057f43b8cdbc001cce9e4537a6683f1bf7af4
Robert Gadon <rgadon107@gmail.com>
2020-02-12 09:35:41 -0600

    Add shortcode handler and view file.

---
+++ src/shortcode.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *  Shortcode handler.
+ *
+ * @package    spiralWebDB\Sandhills
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @link       http://spiralwebdb.com
+ * @license    GNU General Public License 2.0+
+ */
+namespace spiralWebDB\Sandhills;
+
+/*
+ * Register a WordPress shortcode
+ *
+ * @since 1.0.0
+ */
+add_shortcode( 'feature' , __NAMESPACE__ . '\process_feature_shortcode' );
+
+/*
+ * Processing callback for the [feature] shortcode.
+ *
+ * @since 1.0.0.
+ */
+function process_feature_shortcode() {
+
+	ob_start();
+	include '/view/shortcode.php';
+	ob_get_clean();
+}
+
+global $shortcode_tags;
+dump( $shortcode_tags );
---
+++ src/view/shortcode.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  View file for the registered shortcode.
+ *
+ * @package    spiralWebDB\Sandhills;
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @link       http://spiralwebdb.com
+ * @license    GNU General Public License 2.0+
+ */
+namespace spiralWebDB\Sandhills;
+?>
+<p>You called the shortcode view file.</p>
